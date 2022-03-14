73d636f3c87619c71b0eb429add173c944b7a26e
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-03-14 11:16:09 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,80 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+//console.log("Here's the data you're working with");
+//console.log({ CATEGORIES, TASKS });
 
 function App() {
+  //for NewTaskForm
+  const [taskList, setTaskList] = useState([...TASKS]);
+  const [taskCategory, setTaskCategory] = useState("Code");
+  const [itemText, setItemText] = useState("");
+
+  const [categories, setCategories] = useState([...CATEGORIES]);
+  
+  //for CategoryFilter
+  const [selectedCategory, setSelectedCategory] = useState("All")
+ 
+  //sets the category for adding a task 
+  function handleNewFormChangeCat(event){
+    setTaskCategory(event.target.value);
+  }
+
+  //sets the text for adding a task
+  function handleItemText(event){
+    setItemText(event.target.value);
+  }
+
+  //Adding a new item to task list
+  function handleSubmit(event){
+    event.preventDefault();
+    const newTask = {key: taskList.size, text: itemText, category: taskCategory};
+    setTaskList([...taskList, newTask]);
+    setItemText("");
+    setTaskCategory("Code");
+  }
+
+  //set the filter to display tasks 
+  function handleCategoryChange(event){
+    setSelectedCategory(event.target.textContent)
+    event.target.className = "selected";
+  }
+
+  //deletes a task from the list
+  function topLevelDelete(taskToFilter){
+    //console.log(taskToFilter);
+    setTaskList([...taskToFilter]);
+  }
+
+//filters list if a filter is chosen
+  const tasksToDisplay = taskList.filter(task => {
+    if(selectedCategory === "All") return true;
+    else return task.category === selectedCategory;
+  })
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+
+      <CategoryFilter 
+      categories={categories} 
+      handleCategoryChange={handleCategoryChange}
+      />
+
+      <NewTaskForm categories={categories} 
+      itemText={itemText} 
+      handleItemText={handleItemText}
+      handleNewFormChangeCat={handleNewFormChangeCat}
+      onTaskFormSubmit={handleSubmit}
+      />
+
+      <TaskList 
+      tasks={tasksToDisplay}
+      topLevelDelete={topLevelDelete}
+      />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
+function TaskList({tasks, topLevelDelete}) {
+
+  function handleDelete(textToDelete){
+      const taskListFilter = tasks.filter(task => {
+       if(task.text === textToDelete) return false;
+       else return true;
+      }) 
+      topLevelDelete(taskListFilter);
+  }
+
+  const taskList = tasks.map(task => {
+    return (<Task key={tasks.indexOf(task)} text={task.text} category={task.category} handleDelete={handleDelete}/>)
+  })
+
+  return (  
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskList}
     </div>
   );
 }
