b16dfdb85ab541fa059363e4b797857ef9d14267
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-03-14 11:11:00 -0500

    commit tree aa25b2f0f65ab3bc5efe4528a81e492b1981ab49

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, handleCategoryChange}) {
+
+  const categoryList = categories.map(category => {
+    return (<button key={categories.indexOf(category)} onClick={handleCategoryChange}>{category}</button>)
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryList}
     </div>
   );
 }
